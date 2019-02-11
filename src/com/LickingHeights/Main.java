3de121c3d722c76087c47b92e83a1cf5cc5db187
Karen <karen.bandong.2018@student.lhschools.org>
2019-02-11 07:37:17 -0500

    Answers for the users encoded.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -42,9 +42,37 @@ public class Main {
         question= keyboard.nextLine();
         System.out.println("Now please give me a number from 1-100");
         number= keyboard.nextInt();
-        if (number>= 10){
-            System.out.println("That's a great idea!");
+        if (number>= 1 && number<=10){
+            System.out.println("Most likely");
         }
+        if (number>=11 && number<=20){
+            System.out.println("Don't count on it");
+        }
+        if (number>=21 && number<=30){
+            System.out.println("Yes!");
+        }
+        if (number>=31 && number<=40){
+            System.out.println("No");
+        }
+        if (number>=41 && number<=50){
+            System.out.println("Definitely!");
+        }
+        if (number>=51 && number<=60){
+            System.out.println("Absolutely!");
+        }
+        if (number>=61 && number<=70){
+            System.out.println("Very doubtful");
+        }
+        if (number>=71 && number<=80){
+            System.out.println("I will say NO to this");
+        }
+        if (number>=81 && number<=90){
+            System.out.println("Outlook not so great");
+        }
+        if (number>=91 && number<=100){
+            System.out.println("It is certain");
+        }
+
 
         // Ask them their name
         // Make them ask a yes/no question
