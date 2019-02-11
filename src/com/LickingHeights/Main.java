d297029af76a6d2b6d6ce322ffac026223dcfb05
Karen <karen.bandong.2018@student.lhschools.org>
2019-02-11 07:51:14 -0500

    "Else" added.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -42,34 +42,34 @@ public class Main {
         question= keyboard.nextLine();
         System.out.println("Now please give me a number from 1-100");
         number= keyboard.nextInt();
-        if (number>= 1 && number<=10){
+        else if (number>= 1 && number<=10){
             System.out.println("Most likely");
         }
-        if (number>=11 && number<=20){
+        else if (number>=11 && number<=20){
             System.out.println("Don't count on it");
         }
-        if (number>=21 && number<=30){
+        else if (number>=21 && number<=30){
             System.out.println("Yes!");
         }
-        if (number>=31 && number<=40){
+        else if (number>=31 && number<=40){
             System.out.println("No");
         }
-        if (number>=41 && number<=50){
+        else if (number>=41 && number<=50){
             System.out.println("Definitely!");
         }
-        if (number>=51 && number<=60){
+        else if (number>=51 && number<=60){
             System.out.println("Absolutely!");
         }
-        if (number>=61 && number<=70){
+        else if (number>=61 && number<=70){
             System.out.println("Very doubtful");
         }
-        if (number>=71 && number<=80){
+        else if (number>=71 && number<=80){
             System.out.println("I will say NO to this");
         }
-        if (number>=81 && number<=90){
+       else  if (number>=81 && number<=90){
             System.out.println("Outlook not so great");
         }
-        if (number>=91 && number<=100){
+        else if (number>=91 && number<=100){
             System.out.println("It is decidedly so");
         }
 
